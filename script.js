440248abee9632d08f70e188b8267ffb35641c75
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-12 14:24:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
 
-function SportsCar(make, model, topSpeed) {}
+    this.getMakeModel = () => {
+        return this.make + this.model;
+    };
+}
+
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model);  // Correct way to call the parent constructor
+    this.topSpeed = topSpeed;
+
+    this.getTopSpeed = () => {
+        return this.topSpeed;
+    };
+}
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
 // Do not change the code below
 window.Car = Car;
